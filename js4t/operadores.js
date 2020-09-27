72f3c1f18677543a284fc84695a61ae44d8800e7
Lucas Barros <lucasbarros@MacBook-Pro-de-Lucas.local>
2020-09-27 14:53:37 -0400

    Operadores

---
+++ js4t/operadores.js
@@ -0,0 +1,42 @@
+
+// Os operadores vão nos permitir fazer operações, matemáticas, de comparação e lógica.
+
+//Aritiméticos
+//2+2 => 4
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+
+// Comparação
+console.log(2 == 2)
+console.log(2 == 3)
+console.log(2 == "2") // Retorna true pois os valores são iguais independente do tipo
+// Diferente
+console.log(2 != 4/2)
+
+// Igual estrito
+
+console.log(2 === "2") // retorna false, pois verifica, valor e tipo.
+
+
+// Diferente estrito
+
+console.log(3 !== "3") // retorna false, pois verifica, valor e tipo.
+
+// Maior que >
+console.log(2>3)
+console.log(2>1)
+
+// Maior igual >=
+console.log(2>=3)
+console.log(2>=2)
+
+
+// Menor que <
+console.log(2<3)
+console.log(2<1)
+
+// Menor igual <=
+console.log(2<=3)
+console.log(2<=2)
